4e58b2d33579d94ecbfe92e2418f7d1b12e27f86
NaveenMONK <naveenbalaji786@gmail.com>
2021-12-24 15:51:57 +0530

    Final version

---
+++ script.js
@@ -0,0 +1,24 @@
+import countryList from './data.js';
+
+const search = document.querySelector(".search");
+const list = document.querySelector(".list");
+
+let output = "";
+countryList.forEach((country) => {
+  output += `<li>${country}</li>`;
+});
+
+list.innerHTML = output;
+
+search.addEventListener("input", () => {
+  const inputValue = search.value;
+  const li = document.querySelectorAll("li");
+
+  for (let i = 0; i < li.length; i++) {
+    if (li[i].innerHTML.toLowerCase().includes(inputValue)) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+});
